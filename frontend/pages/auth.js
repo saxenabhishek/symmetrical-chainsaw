f471d26ae9349a0c191d5731734f03a83d73e85b
saxenabhishek <as7122000@gmail.com>
2021-05-10 02:44:40 +0530

    🚧  auth page

--- frontend/pages/auth.js
+++ frontend/pages/auth.js
@@ -1,3 +1,47 @@
 export default function Auth(props) {
-  return <h1>Auth page</h1>;
+  return (
+    <section class="text-gray-400 bg-gray-900 body-font h-screen">
+      <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
+        <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+          <h1 class="title-font font-medium text-3xl text-white">
+            Some smart sounding long lines of bs.
+          </h1>
+          <p class="leading-relaxed mt-4">More bs to support the bs above</p>
+        </div>
+        <div class="lg:w-2/6 md:w-1/2 bg-gray-800 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+          <h2 class="text-white text-lg font-medium title-font mb-5">
+            Sign in
+          </h2>
+          <div class="relative mb-4">
+            <label for="full-name" class="leading-7 text-sm text-gray-400">
+              Username
+            </label>
+            <input
+              type="text"
+              id="fu"
+              name="full-name"
+              class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div class="relative mb-4">
+            <label for="email" class="leading-7 text-sm text-gray-400">
+              Password
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Button
+          </button>
+          <p class="text-xs mt-3">
+            Literally you probably haven't heard of them jean shorts.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
