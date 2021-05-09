b7ed1a627cf123cd9bdc79c3d514b533de722205
saxenabhishek <as7122000@gmail.com>
2021-05-09 20:19:07 +0530

    ✨ Context token

---
+++ frontend/components/Context/Auth.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useEffect } from "react";
+
+export const tokencontext = createContext();
+
+const getToken = () => {
+  const tokenString = sessionStorage.getItem("token");
+  const userToken = JSON.parse(tokenString);
+  console.log(userToken);
+  return userToken;
+};
+
+export default function Tokenprovider(props) {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const tokenString = sessionStorage.getItem("token");
+    const userToken = JSON.parse(tokenString);
+    setToken(userToken);
+  }, []);
+
+  const saveToken = (userToken) => {
+    sessionStorage.setItem("token", JSON.stringify(userToken));
+    console.log(userToken);
+    setToken(userToken);
+  };
+
+  return (
+    <tokencontext.Provider value={{ token, setToken: saveToken }}>
+      {props.children}
+    </tokencontext.Provider>
+  );
+}
