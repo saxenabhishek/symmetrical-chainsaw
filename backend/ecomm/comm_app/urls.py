72b8b0cb3e1536ab4cc6f55ab0d4127ea8451b95
Aradhya-Tripathi <at8029@srmist.edu.in>
2021-05-10 00:45:22 +0530

    feat(perms): added permission checks addressing #6

--- backend/ecomm/comm_app/urls.py
+++ backend/ecomm/comm_app/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='comm_login'),
     path('products/', views.products, name="products"),
-    path('signup/', views.register, name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('login/', views.loginuser, name="login"),
     path('logout/', views.logoutuser, name="logout"),
 ]
--- backend/ecomm/comm_app/views.py
+++ backend/ecomm/comm_app/views.py
@@ -1,31 +1,32 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, resolve_url
 from django.shortcuts import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
+from .decorators import auth_redirect
+from django.contrib.auth.decorators import login_required
+from .decorators import check_perms
 
 
-def home(request):
-    return HttpResponse('Home')
-
-
+@check_perms
 def products(request):
     return HttpResponse('Products')
 
 
-def register(request):
+@auth_redirect
+def signup(request):
     if request.method == "POST":
         username = request.POST['username']
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
-        # checks
         user = User.objects.create_user(username, email, password1, password2)
         user.save()
-        return redirect('login/')
+        return redirect(resolve_url('login'))
     return HttpResponse('404 - Not Found')
 
 
+@auth_redirect
 def loginuser(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -35,13 +36,14 @@ def loginuser(request):
 
         if user is not None:
             login(request, user)
-            return redirect('products/')
+            return redirect(resolve_url('products'))
 
         return HttpResponse('Invalid credentials')
 
     return HttpResponse('404-Page Not Found')
 
 
+@login_required(login_url="login/")
 def logoutuser(request):
     logout(request)
     return redirect('signup')
