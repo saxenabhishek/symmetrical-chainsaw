fad0e0739056c81beae012d5dfee63b602224cfa
saxenabhishek <as7122000@gmail.com>
2021-05-14 14:52:00 +0530

    ✨ Sign in post route

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,44 +1,63 @@
-import { useEffect, useState } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useState } from "react";
+import { Formik, Form } from "formik";
 import Link from "next/link";
+import Router from "next/router";
 import FormField from "../components/formField";
 
 export default function signin() {
+  let [text, settext] = useState("");
   return (
     <section className="text-gray-400 bg-gray-900 body-font h-full md:h-screen">
       <div className="container px-5 py-24 mx-auto items-center">
         <Formik
-          initialValues={{ Email: "", Password: "", Username: "", Phone: "" }}
-          //   validate={(values) => {
-          //     console.log(values);
-          //     const errors = {};
-          //     for (const [key, value] of Object.entries(values)) {
-          //       if (!value) {
-          //         errors[key] = "Required";
-          //       }
-          //     }
-          //     if (
-          //       !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          //     ) {
-          //       errors.Email = "Invalid email address";
-          //     }
-          //     console.log(errors);
-          //     return errors;
-          //   }}
+          initialValues={{ email: "", password: "", username: "" }}
+          validate={(values) => {
+            const errors = {};
+            for (const [key, value] of Object.entries(values)) {
+              if (!value) {
+                errors[key] = "Required";
+              }
+            }
+            if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            return errors;
+          }}
           onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
+            settext("Sending...");
+            let x = {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(values),
+            };
+            console.log(x);
+            fetch("http://localhost:8000/apis/register", x)
+              .then((res) => {
+                if (res.status == 201) {
+                  settext("User account registered");
+                  console.log("Created :)))))");
+                  Router.replace("/sign");
+                } else {
+                  settext("Sorry try again :p");
+                  Router.replace("/signup");
+                }
+              })
+              .catch((error) => {
+                console.log(error);
+              });
           }}
         >
           {({ isSubmitting }) => (
             //   username
             <Form className="max-w-lg bg-gray-800 bg-opacity-50 rounded-lg ring ring-indigo-400 p-8 flex flex-col mx-auto w-full mt-10 md:mt-0">
-              <FormField name="Username" type="name" />
-              <FormField name="Password" type="password" />
-              <FormField name="Email" type="email" />
-              <FormField name="Phone" type="Phone" />
+              {text && <p className="text-indigo-500">{text}</p>}
+              <FormField name="username" type="name" />
+              <FormField name="password" type="password" />
+              <FormField name="email" type="email" />
               <button
                 className=" text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
                 type="submit"
@@ -50,7 +69,6 @@ export default function signin() {
                 <p className="text-xs mt-3 text-white">
                   Already have a account?
                 </p>
-                {/* <p className="text-blue-500">No?</p> */}
               </Link>
             </Form>
           )}
