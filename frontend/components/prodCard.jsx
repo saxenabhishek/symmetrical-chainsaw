76b3a65614501ca28280f24a6cf101929b3bb5fa
saxenabhishek <as7122000@gmail.com>
2021-05-31 15:32:13 +0530

    ♻️ use products raw

--- frontend/components/prodCard.jsx
+++ frontend/components/prodCard.jsx
@@ -1,30 +1,43 @@
 import Image from "next/image";
+import { useCart } from "./Context/Cart";
 
-export default function Card({ alt, src, title, subtitle, price }) {
+export default function Card({ alt, src, title, subtitle, price, p }) {
+  const cart = useCart();
+  let na = p.name;
+  let names = na.substring(1, na.length - 1).split(",");
+  let smart = names.map((n, i) => {
+    return n.substring(1, n.length - 1);
+  });
+  console.log(p, smart);
   return (
-    <div className="p-4 w-full bg-gray-800 border-r-2 border-b-2 border-green-700 rounded-xl ">
-      <a className="block relative h-48 bg-white rounded overflow-hidden">
-        <Image
-          alt={alt}
-          class="object-contain object-center md:object-scale-down  w-full h-full block"
-          src={src}
-          //   height={421}
-          //   width={262}
-          layout="fill"
-        />
-      </a>
-      <div className="mt-4">
-        <h3 className="text-white text-xs tracking-widest title-font mb-1">
-          {subtitle}
-        </h3>
-        <h2 className="text-white title-font text-lg font-medium">{title}</h2>
-        <div className="flex justify-between">
-          <p className="mt-1 text-green-400">Price: ₹{price}</p>
-          <button className="transition-colors transform hover:bg-green-800 mx-5 bg-green-400 px-2 py-1 text-white rounded-md">
-            Add
-          </button>
+    <div className="Container">
+      <div className="p-4 w-full bg-gray-800 border-r-2 border-green-700 rounded-xl rounded-b-none ">
+        <a className="block relative h-48 bg-white rounded overflow-hidden">
+          <Image
+            alt={smart[0]}
+            className="object-contain object-center md:object-scale-down  w-full h-full block"
+            src={p.img_src}
+            //   height={421}
+            //   width={262}
+            layout="fill"
+          />
+        </a>
+        <div className="mt-4">
+          <h3 className="text-white text-xs tracking-widest title-font mb-1">
+            {smart[0]}
+          </h3>
+          <h2 className="text-white title-font text-lg font-medium">
+            {smart[1]}
+          </h2>
+          <p className="mt-1 text-green-400">Price: ₹{p.price}</p>
         </div>
       </div>
+      <button
+        onClick={() => cart.addItem(p)}
+        className="font-semibold w-full font-sans hover:bg-green-800  bg-green-400 px-2 py-3 text-white rounded-xl rounded-t-none"
+      >
+        Add
+      </button>
     </div>
   );
 }
