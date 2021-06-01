6dee1ebdb54f3b988657dc9447bdcb3b2c6cd49d
saxenabhishek <as7122000@gmail.com>
2021-06-02 01:38:46 +0530

    ✨ blur when added

--- frontend/components/prodCard.jsx
+++ frontend/components/prodCard.jsx
@@ -1,17 +1,32 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { useCart } from "./Context/Cart";
 
 export default function Card({ alt, src, title, subtitle, price, p }) {
   const cart = useCart();
+  const [taken, setTaken] = useState(false);
+  useEffect(() => {
+    for (let i = 0; i < cart.cart.length; i++) {
+      if (p.img_src == cart.cart[i].img_src) {
+        setTaken(true);
+        break;
+      }
+    }
+  }, []);
+
   let na = p.name;
   let names = na.substring(1, na.length - 1).split(",");
   let smart = names.map((n, i) => {
     return n.substring(1, n.length - 1);
   });
-  console.log(p, smart);
   return (
-    <div className="Container">
-      <div className="p-4 w-full bg-gray-800 border-r-2 border-green-700 rounded-xl rounded-b-none ">
+    <div className="Container flex-auto m-5">
+      <div
+        className={
+          (taken ? "filter blur-sm " : " ") +
+          "p-4 w-full bg-gray-800 border-r-2 border-green-700 rounded-xl rounded-b-none"
+        }
+      >
         <a className="block relative h-48 bg-white rounded overflow-hidden">
           <Image
             alt={smart[0]}
@@ -27,17 +42,34 @@ export default function Card({ alt, src, title, subtitle, price, p }) {
             {smart[0]}
           </h3>
           <h2 className="text-white title-font text-lg font-medium">
-            {smart[1]}
+            {smart[1].length > 30 ? smart[1].substring(0, 30) + ".." : smart[1]}
           </h2>
           <p className="mt-1 text-green-400">Price: ₹{p.price}</p>
         </div>
       </div>
-      <button
-        onClick={() => cart.addItem(p)}
-        className="font-semibold w-full font-sans hover:bg-green-800  bg-green-400 px-2 py-3 text-white rounded-xl rounded-t-none"
+      <div
+        onClick={() => {
+          console.log("click");
+          if (taken) {
+            setTaken(false);
+            cart.removeItem(p);
+          } else {
+            setTaken(true);
+
+            cart.addItem(p);
+          }
+          console.log(taken);
+        }}
+        className={
+          (taken
+            ? "bg-red-800 hover:bg-red-400"
+            : "bg-green-400 hover:bg-green-800") +
+          " " +
+          "font-semibold w-full font-sans text-center px-2 py-3 text-white rounded-xl rounded-t-none"
+        }
       >
-        Add
-      </button>
+        {taken ? "Remove" : "Add"}
+      </div>
     </div>
   );
 }
