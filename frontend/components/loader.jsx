913eeec7cc75d2d6463d8406b8afc1e78e8a0c78
saxenabhishek <as7122000@gmail.com>
2021-06-02 01:45:38 +0530

    ✨ black backgroud

--- frontend/components/loader.jsx
+++ frontend/components/loader.jsx
@@ -1,6 +1,6 @@
 export default function Loader() {
   return (
-    <div className="flex h-screen">
+    <div className="flex bg-gray-800 h-screen">
       <div className="m-auto">
         <div className="w-48 h-48 rounded-full bg-purple-400 animate-ping opacity-75"></div>
       </div>
